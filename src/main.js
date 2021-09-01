1d6a2816d78edd854e06048e6eac601c18e4ae00
claxrich <30460697+ntsw2001@users.noreply.github.com>
2021-09-01 11:13:46 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,16 +3,27 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import ElementUI from 'element-ui';
+
+import 'element-ui/lib/theme-chalk/index.css';
 import 'normalize.css/normalize.css';
 import './styles/init.css';
-import 'element-ui/lib/theme-chalk/index.css';
+import './styles/font.css'
+
 import 'amfe-flexible';
 import 'amfe-flexible/index.js';
 
+// 自定义封装后的库
+import auth from '@/utils/auth';
+import http from '@/utils/http';
+import crypt from '@/utils/crypt';
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
+
+Vue.prototype.$auth = auth;
+Vue.prototype.$http = http;
+Vue.prototype.$crypt = crypt;
 
 new Vue({
   router,
